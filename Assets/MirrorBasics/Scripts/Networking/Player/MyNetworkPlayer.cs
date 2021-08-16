93688d9b34bc4ef31fa52c3979d261be09d0f18f
Ben James <39443778+Benjames01@users.noreply.github.com>
2021-08-16 18:55:48 +0100

    Add Username Validation
    Add username validation on setting name

--- Assets/MirrorBasics/Scripts/Networking/Player/MyNetworkPlayer.cs
+++ Assets/MirrorBasics/Scripts/Networking/Player/MyNetworkPlayer.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using Mirror;
 using TMPro;
 using UnityEngine;
@@ -9,7 +10,6 @@ public class MyNetworkPlayer : NetworkBehaviour
 {
     [SerializeField] private TMP_Text displayNameText = null;
     [SerializeField] private Renderer displayColourRenderer = null;
-    
 
     [SyncVar(hook = nameof(HandleDisplayNameUpdated))]
     [SerializeField] private string displayName = "Missing Name";
@@ -17,7 +17,7 @@ public class MyNetworkPlayer : NetworkBehaviour
     [SerializeField] private Color teamColour = new Color();
 
 
-    #region Server
+    #region Server.
     [Server]
     public void SetDisplayName(string newDisplayName)
     {
@@ -29,10 +29,25 @@ public class MyNetworkPlayer : NetworkBehaviour
     {
         teamColour = newTeamColour;
     }
+
+    [Command]
+    private void CmdSetDisplayName(string newDisplayName)
+    {
+        if (!IsValidName(newDisplayName)) return;
+        RpcLogNewName(newDisplayName);
+        SetDisplayName(newDisplayName);
+    }
+    
     #endregion
 
-    #region Private
+    #region Client.
 
+    [ClientRpc]
+    private void RpcLogNewName(string newDisplayName)
+    {
+        Debug.Log($"Setting display name to {newDisplayName}");
+    }
+    
     private void HandleDisplayColourUpdated(Color oldColour, Color newColour)
     {
         displayColourRenderer.material.SetColor("_Color", newColour);
@@ -43,6 +58,26 @@ public class MyNetworkPlayer : NetworkBehaviour
         displayNameText.text = newName;
     }
 
+    [ContextMenu("Set My Name")]
+    private void SetMyName()
+    {
+        CmdSetDisplayName("M");
+    }
+
     #endregion
 
+
+    #region Utilities
+
+    public static bool IsValidName(string nameInput)
+    {
+        bool isValid = true;
+        isValid = !string.IsNullOrEmpty(nameInput)
+                  && Regex.IsMatch(nameInput, @"^[a-zA-Z]+$")
+                  && nameInput.Length > 2;
+        return isValid;
+    }
+    
+
+    #endregion
 }
